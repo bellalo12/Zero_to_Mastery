f279bc85ce2b8146dc62f16543157c65711b7c56
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-04-07 12:56:14 +0800

    adding background color and changing text-family

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends React.Component{
   constructor(){
@@ -20,7 +21,7 @@ class App extends React.Component{
     })
     return(
       <div className='tc'>
-      <h1>RoboFriends</h1>
+      <h1 className='f2'>RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange}/>
       <CardList robots={filterRobot}/>
       </div>
