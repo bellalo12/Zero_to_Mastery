e4fbb0ec1f70aff7ed8a3f17feedc8570e537488
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-04-08 14:07:54 +0800

    changing bundle.js to script.js

--- BackgroundGenerator/script.js
+++ BackgroundGenerator/script.js
@@ -1,13 +1,13 @@
-var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient')
+var h3 = document.querySelector('h3');
 
 
 
 function changeColor(){
   body.style.background ="linear-gradient(to right, "+color1.value+", " +color2.value+")";
-  css.textContent = body.style.background+ ";";
+  h3.textContent = body.style.background+ ";";
 }
 
 color1.addEventListener('input', changeColor);
