9676cbb29419f6f23ffa75c3d9778350d4a3b91d
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-04-08 13:18:13 +0800

    adding bg-color to robofriends

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends React.Component{
   constructor(){
     super()
     this.state={
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(user=>this.setState({robots: user}))
   }
   onSearchChange =(event)=>{
     this.setState({searchfield: event.target.value})
@@ -19,18 +24,21 @@ class App extends React.Component{
     const filterRobot = this.state.robots.filter(robot=>{
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
+    if(this.state.robots.length===0){
+      return <h1>Loading</h1>
+    } else{
     return(
       <div className='tc'>
       <h1 className='f2'>RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange}/>
+      <Scroll>
       <CardList robots={filterRobot}/>
+      </Scroll>
       </div>
-    )
+    );
+  }
   }
 }
-
-
-
 
 
 
