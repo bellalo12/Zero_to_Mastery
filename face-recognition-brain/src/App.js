fa4690cdf3820cf8bd47023f30087f1f0458b65e
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-04-14 20:23:33 +0800

    Adding bounding-box

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import './App.css';
+
+
+const app = new Clarifai.App({
+ apiKey:'f9d8f19378684c46bf17d029cabed99f'
+});
 
 const particlesOptions ={
   particles: {
@@ -19,6 +26,45 @@ const particlesOptions ={
 }
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+      box:{}
+    }
+  }
+
+  calculateFaceSize=(data)=>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      topRow: clarifaiFace.top_row * height,
+      leftCol: clarifaiFace.left_col * width,
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+      rightCol:  width - (clarifaiFace.right_col * width)
+    }
+  }
+
+  displayFaceBox=(box)=>{
+    this.setState({box: box})
+  }
+
+  onInputChange=(event)=>{
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit=()=>{
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response=> this.displayFaceBox(this.calculateFaceSize(response)))
+      .catch(err=>console.log(err))
+    }
+
   render(){
     return(
       <div className='App'>
@@ -28,9 +74,10 @@ class App extends React.Component{
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />*/}
+      <ImageLinkForm
+      onInputChange={this.onInputChange}
+      onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- face-recognition-brain/src/components/ImageLinkForm/ImageLinkForm.js
+++ face-recognition-brain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm=()=>{
+const ImageLinkForm=({onInputChange, onButtonSubmit})=>{
   return(
     <div>
     <p className='f3 tc'>
@@ -10,8 +10,10 @@ const ImageLinkForm=()=>{
     </p>
     <div className='center'>
     <div className='form center pa4 br3 shadow-5'>
-    <input className='w-70 f4 pa2' type='tex' />
-    <button className='f4 white pv2 link ph3 grow dib bg-light-red w-30'>Detect</button>
+    <input className='w-70 f4 pa2' type='tex' onChange={onInputChange}/>
+    <button
+    className='f4 white pv2 link ph3 grow dib bg-light-red w-30'
+    onClick={onButtonSubmit}>Detect</button>
     </div>
     </div>
     </div>
